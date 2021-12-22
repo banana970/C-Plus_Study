df2b64548cc0bb978c113b5b2444f0f5500a8fa2
seonpilKim <ksp970306@gmail.com>
2021-12-22 21:48:54 +0900

    Solve: [S4] 연속부분최대곱

---
+++ 선필/21.12. - 4주차/2670.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <bitset>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <numeric>
+#include <functional>
+#include <sstream>
+#include <cctype>
+
+using namespace std;
+
+const long long MOD = 1e9 + 7;
+const long long INF = 1e9;
+
+const int dr[] = {1, 0, -1, 0, 1, 1, -1, -1};
+const int dc[] = {0, 1, 0, -1, 1, -1, 1, -1};
+const int dh[] = {1, -1};
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int N;
+	cin >> N;
+	vector<double> v(N);
+	for (int i = 0; i < N; i++)
+		cin >> v[i];
+
+	double ans = 0;
+
+	for (int i = 0; i < N; i++) {
+		double cur = 1;
+		for (int j = i; j < N; j++) {
+			cur *= v[j];
+			ans = max(ans, cur);
+		}
+	}
+
+	cout.precision(3);
+	cout << fixed << ans;
+
+	return 0;
+}
