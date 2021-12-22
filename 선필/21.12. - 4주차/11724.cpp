8666a3a3b474e78ba83a7dfd872092ffaf50e399
seonpilKim <ksp970306@gmail.com>
2021-12-22 21:49:24 +0900

    Solve: [S2] 연결 요소의 개수

---
+++ 선필/21.12. - 4주차/11724.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <bitset>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <numeric>
+#include <functional>
+#include <sstream>
+#include <cctype>
+
+using namespace std;
+
+const long long MOD = 1e9 + 7;
+const long long INF = 1e9;
+
+const int dr[] = {1, 0, -1, 0, 1, 1, -1, -1};
+const int dc[] = {0, 1, 0, -1, 1, -1, 1, -1};
+const int dh[] = {1, -1};
+
+int ans;
+vector<int> par;
+
+int find(int u) {
+	if (u == par[u])
+		return u;
+	return par[u] = find(par[u]);
+}
+
+void merge(int u, int v) {
+	u = find(u);
+	v = find(v);
+
+	if (u == v)
+		return;
+
+	ans--;
+	par[v] = u;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int N, M;
+	cin >> N >> M;
+	ans = N;
+	par.resize(N + 1);
+	for (int i = 1; i <= N; i++)
+		par[i] = i;
+
+	while (M--) {
+		int u, v;
+		cin >> u >> v;
+
+		merge(u, v);
+	}
+
+	cout << ans;
+
+	return 0;
+}
